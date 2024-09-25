b655ec34ac8ce8794850ffb54661b7fbda9411f1
dipti0211 <145998610+dipti0211@users.noreply.github.com>
2024-09-25 22:25:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,85 @@
+$(window).load(function() {
+    $("#preloader").fadeOut("slow");
+});
+
+$(document).ready(function(){
+
+    wow = new WOW({
+        mobile:       false,       // default
+      }
+    )
+    wow.init();
+
+     $('#top-nav').onePageNav({
+        currentClass: 'current',
+        changeHash: true,
+        scrollSpeed: 1200
+    });
+
+     
+    //animated header class
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 100) {
+            $(".navbar-default").addClass("animated");
+        } else {
+            $(".navbar-default").removeClass('animated');
+        }
+    });
+
+    $('#countdown_dashboard').countDown({
+        targetDate: {
+            'day':      11,
+            'month':    3,
+            'year':     2017,
+            'hour':     00,
+            'min':      00,
+            'sec':      01,
+        },
+        omitWeeks: true
+    });
+
+    $('.init-slider').owlCarousel({
+        items:1,
+        merge:true,
+        loop:true,
+        video:true,
+        smartSpeed: 600
+    });
+
+    $("#contact-form").validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            message: {
+                required: true,
+                minlength: 2
+            },
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Please enter Your Name",
+                minlength: "Your name must consist of at least 2 characters"
+            },
+            message: {
+                required: "Please Write Something",
+                minlength: "Your message must consist of at least 2 characters"
+            },
+            email: "Please enter a valid email address"
+        },
+        submitHandler: function(form) {
+           form.name.value="";
+           form.email.value="";
+           form.message.value="";
+           $("#success").fadeIn();
+           
+        }
+    });
+
+});
+
